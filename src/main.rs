6092896fa7c0ad824f51c566efee85ab2bae640c
Scott Minor <minor@minorhacks.com>
2021-04-19 16:59:21 -0700

    Clean up main; add ingest subcommand

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,6 @@ extern crate tokio;
 
 use fantasy_chess::analysis;
 use fantasy_chess::api;
-use futures::StreamExt;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -61,6 +60,42 @@ async fn main() -> anyhow::Result<()> {
     .author("Scott Minor <minor@minorhacks.com>")
     .about("CLI for fantasy-chess related operations")
     .subcommand(
+      clap::SubCommand::with_name("ingest")
+        .about("pull game(s) and ingest into a database")
+        .group(
+          clap::ArgGroup::with_name("source")
+            .args(&["chess_com_game_id", "lichess_game_id"])
+            .required(true),
+        )
+        .group(
+          clap::ArgGroup::with_name("db")
+            .args(&["sqlite_db_file", "mysql_db"])
+            .required(true),
+        )
+        .arg(
+          clap::Arg::with_name("chess_com_game_id")
+            .help("ID of the game on chess.com")
+            .long("chess_com_game_id"),
+        )
+        .arg(
+          clap::Arg::with_name("lichess_game_id")
+            .help("ID of the game on Lichess")
+            .long("lichess_game_id"),
+        )
+        .arg(
+          clap::Arg::with_name("sqlite_db_file")
+            .help(
+              "Path to sqlite DB file. Will be created if it doesn't exist.",
+            )
+            .long("sqlite_db_file"),
+        )
+        .arg(
+          clap::Arg::with_name("mysql_db")
+            .help("MySQL DB connection string")
+            .long("mysql_db"),
+        ),
+    )
+    .subcommand(
       clap::SubCommand::with_name("score_game")
         .about("scores an individual game by chess.com ID")
         .arg(
@@ -69,69 +104,33 @@ async fn main() -> anyhow::Result<()> {
             .required(true),
         ),
     )
-    .subcommand(
-      clap::SubCommand::with_name("score_player")
-        .about("scores and averages the last N games for a player by name")
-        .arg(
-          clap::Arg::with_name("chess.com username")
-            .help("Username of the player on chess.com")
-            .required(true),
-        )
-        .arg(
-          clap::Arg::with_name("num games")
-            .help("Number of games to analyze")
-            .short("n")
-            .long("num_games")
-            .takes_value(true),
-        ),
-    )
     .get_matches();
 
-  if let Some(score_game_args) = matches.subcommand_matches("score_game") {
-    // Fetch chess game by ID from chess.com
-    let chess_id = score_game_args.value_of("chess.com ID").unwrap();
-    let uri = format!("https://www.chess.com/callback/live/game/{}", chess_id);
-    let body = reqwest::get(&uri).await?.text().await?;
-    let res: api::GameResponse = serde_json::from_str(&body)?;
-    // API documentation:
-    // https://github.com/andyruwruw/chess-web-api/blob/master/documentation/GAME.md
-    // moveList parsing:
-    // https://github.com/andyruwruw/chess-web-api/issues/10#issuecomment-779735204
-    // Calculate which pieces capture how many points
-    // Promotion info:
-    // https://github.com/andyruwruw/chess-web-api/issues/11#issuecomment-783687021
-    let score = analysis::score_game(&res.game)?;
-    // Print output
-    println!("{}", score);
-  } else if let Some(score_player_args) =
-    matches.subcommand_matches("score_player")
-  {
-    // Get the player's ID
-    let uri = format!(
-      "https://api.chess.com/pub/player/{}",
-      score_player_args.value_of("chess.com username").unwrap()
-    );
-    let body = reqwest::get(&uri).await?.text().await?;
-    let res: api::PlayerResponse = serde_json::from_str(&body)?;
-    // Look up their games
-    let uri = format!("https://www.chess.com/callback/user/daily/archive?all=1&locale=en_US&userId={}", res.player_id);
-    let body = reqwest::get(&uri).await?.text().await?;
-    let res: Vec<api::GameSummary> = serde_json::from_str(&body)?;
-    let fetches =
-      futures::stream::iter(res.into_iter().map(|summary| async move {
-        let uri =
-          format!("https://www.chess.com/callback/live/game/{}", summary.id);
-        let body = reqwest::get(&uri).await.unwrap().text().await.unwrap();
-        let res: api::GameResponse = serde_json::from_str(&body).unwrap();
-        res.game
-      }))
-      .buffer_unordered(3)
-      .collect::<Vec<api::Game>>()
-      .await;
-    println!("{:?}", analysis::average_games(&fetches)?);
-    // https://www.chess.com/callback/user/daily/archive?all=1&locale=en_US&userId=31513926
-  } else {
-    unimplemented!("command not implemented");
+  match matches.subcommand() {
+    ("score_game", Some(score_game_args)) => {
+      // Fetch chess game by ID from chess.com
+      let chess_id = score_game_args.value_of("chess.com ID").unwrap();
+      let uri =
+        format!("https://www.chess.com/callback/live/game/{}", chess_id);
+      let body = reqwest::get(&uri).await?.text().await?;
+      let res: api::GameResponse = serde_json::from_str(&body)?;
+      // API documentation:
+      // https://github.com/andyruwruw/chess-web-api/blob/master/documentation/GAME.md
+      // moveList parsing:
+      // https://github.com/andyruwruw/chess-web-api/issues/10#issuecomment-779735204
+      // Calculate which pieces capture how many points
+      // Promotion info:
+      // https://github.com/andyruwruw/chess-web-api/issues/11#issuecomment-783687021
+      let score = analysis::score_game(&res.game)?;
+      // Print output
+      println!("{}", score);
+    }
+    ("ingest", Some(_ingest_args)) => {
+      todo!("implement ingest")
+    }
+    _ => {
+      unimplemented!("command not implemented")
+    }
   }
   Ok(())
 }
