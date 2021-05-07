ed89b1f1cffd518043f394efca180eefc9634759
Scott Minor <minor@minorhacks.com>
2021-05-06 22:57:09 -0700

    Reliably adds games to SQLite
    No longer have pool timeouts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::sync::Arc;
+use std::{collections::VecDeque, sync::Arc};
 
-use tokio::sync::mpsc::{self, Receiver, Sender};
+use tokio::{
+  sync::mpsc::{self, Receiver, Sender},
+  task::JoinHandle,
+};
 
 use async_stream::stream;
 use fantasy_chess::pgn;
@@ -60,6 +63,16 @@ async fn main() -> anyhow::Result<()> {
             .validator(|s| {
               s.parse::<u32>().map(|_| ()).map_err(|e| e.to_string())
             }),
+        )
+        .arg(
+          clap::Arg::with_name("num_insert_workers")
+            .help("Number of concurrent DB insert tasks")
+            .long("num_insert_workers")
+            .takes_value(true)
+            .default_value("10")
+            .validator(|s| {
+              s.parse::<u32>().map(|_| ()).map_err(|e| e.to_string())
+            }),
         ),
     )
     .get_matches();
@@ -71,14 +84,31 @@ async fn main() -> anyhow::Result<()> {
       if let Some(pgn_filename) = ingest_args.value_of("pgn_file") {
         let f = std::fs::File::open(pgn_filename)?;
 
-        let (tx, mut rx): (Sender<usize>, Receiver<usize>) = mpsc::channel(2);
+        let (completed_queries_tx, mut completed_queries_rx): (
+          Sender<usize>,
+          Receiver<usize>,
+        ) = mpsc::channel(1);
+
         let mut tasks = Vec::new();
+        let mut query_workers_tx = VecDeque::new();
+
+        for _i in 0..ingest_args
+          .value_of("num_insert_workers")
+          .map(|v| v.parse::<u32>().unwrap())
+          .unwrap()
+        {
+          let (task, tx_chan) =
+            start_query_executor(db.clone(), completed_queries_tx.clone());
+          tasks.push(task);
+          query_workers_tx.push_back(tx_chan);
+        }
+        drop(completed_queries_tx);
 
         tasks.push(tokio::spawn(async move {
           let term = console::Term::stderr();
           let mut total_games: usize = 0;
           let mut total_queries: usize = 0;
-          while let Some(query_count) = rx.recv().await {
+          while let Some(query_count) = completed_queries_rx.recv().await {
             total_games += 1;
             total_queries += query_count;
             term.clear_line().unwrap();
@@ -95,17 +125,14 @@ async fn main() -> anyhow::Result<()> {
         let games = game_stream(f).map(insert_queries);
         pin_mut!(games);
         while let Some(queries) = games.next().await {
-          let db = db.clone();
-          let tx = tx.clone();
-          tasks.push(tokio::spawn(async move {
-            let num_queries = queries.len();
-            for query in queries {
-              query.execute(&*db).await.unwrap();
-            }
-            tx.send(num_queries).await.unwrap();
-          }));
+          let tx = query_workers_tx.pop_front().unwrap();
+          tx.send(queries).await.unwrap_or_else(|_| {
+            panic!("failed to send queries to query runner")
+          });
+          query_workers_tx.push_back(tx);
         }
-        drop(tx);
+
+        drop(query_workers_tx);
         join_all(tasks).await;
       } else {
         unimplemented!()
@@ -116,6 +143,31 @@ async fn main() -> anyhow::Result<()> {
     }
   }
   Ok(())
+}
+
+type StaticSqlQuery =
+  sqlx::query::Query<'static, sqlx::Any, sqlx::any::AnyArguments<'static>>;
+
+fn start_query_executor(
+  db: Arc<sqlx::Pool<sqlx::Any>>,
+  completed_queries_tx: mpsc::Sender<usize>,
+) -> (JoinHandle<()>, mpsc::Sender<Vec<StaticSqlQuery>>) {
+  let (parsed_games_tx, mut parsed_games_rx): (
+    mpsc::Sender<Vec<StaticSqlQuery>>,
+    mpsc::Receiver<Vec<StaticSqlQuery>>,
+  ) = mpsc::channel(1);
+
+  let task = tokio::spawn(async move {
+    while let Some(queries) = parsed_games_rx.recv().await {
+      let num_queries = queries.len();
+      for query in queries {
+        query.execute(&*db).await.unwrap();
+      }
+      completed_queries_tx.send(num_queries).await.unwrap();
+    }
+  });
+
+  (task, parsed_games_tx)
 }
 
 async fn connect_to_db(
